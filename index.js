7ec495b1f35bbec2ae1f964054c8ea13877406f2
Chowa <arfinchowa524@gmail.com>
2025-10-07 00:55:07 +0600

    gemini api key added

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express =require("express")
+const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-app.get("/",(req,res)=>{
-    res.send({data:'server Running'})
-})
+app.get("/", (req, res) => {
+  res.send({ data: "server Running" });
+});
 
-app.listen(port),()=>{
-    console.log("server running on port")
+import { GoogleGenAI } from "@google/genai";
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
 }
-app.listen(port),()=>{
-    console.log("server running on port")
-}
+
+main();
+
+app.listen(port),
+  () => {
+    console.log("server running on port");
+  };
