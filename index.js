7d0fffc8b12e8dc9278804d3768695e2635d325e
Chowa <arfinchowa524@gmail.com>
2025-10-08 23:54:03 +0600

    server is running

--- index.js
+++ index.js
@@ -6,20 +6,7 @@ app.get("/", (req, res) => {
   res.send({ data: "server Running" });
 });
 
-import { GoogleGenAI } from "@google/genai";
 
-// The client gets the API key from the environment variable `GEMINI_API_KEY`.
-const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
-
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
-
-main();
 
 app.listen(port),
   () => {
